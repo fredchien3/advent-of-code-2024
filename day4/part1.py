af8f702ea7417b2ba3d863aa36d9dd017d3eb34f
Fred Chien <fred.chien3@gmail.com>
2024-12-04 21:31:42 -0500

    Refactor day 4 part 1

--- day4/part1.py
+++ day4/part1.py
@@ -2,11 +2,22 @@ file = open('input.txt', 'r')
 
 rows = file.read().split('\n')
 
-# these bounds are inclusive
-left_bound = 3
-upper_bound = 3
-right_bound = len(rows[0]) - 4
-lower_bound = len(rows) - 4
+# these bounds are exclusive
+left_bound = 0
+upper_bound = 0
+right_bound = len(rows[0]) - 1
+lower_bound = len(rows) - 1
+
+directions = [
+  [ 0,  1], # right
+  [ 0, -1], # left
+  [-1,  0], # up
+  [ 1,  0], # down
+  [-1,  1], # upright
+  [ 1, -1], # downleft
+  [-1, -1], # upleft
+  [ 1,  1] # downright
+]
 
 # Given coordinates in the input, search all eight directions
 # and return the number of XMASes found.
@@ -14,68 +25,23 @@ def num_xmases(i, j):
   count = 0
   letter = rows[i][j]
 
-  # Check horizontally both ways
-  if j <= right_bound:
-    four_right = []
-    for offset in range(0, 4):
-      four_right.append(rows[i][j + offset])
-    if ''.join(four_right) == 'XMAS':
-      count += 1
-
-  if j >= left_bound:
-    four_left = []
-    for offset in range(0, 4):
-      four_left.append(rows[i][j - offset])
-    if ''.join(four_left) == 'XMAS':
-      count += 1
-
-  # Check vertically both ways
-  if i >= upper_bound:
-    four_above = []
-    for offset in range(0, 4):
-      four_above.append(rows[i - offset][j])
-    if ''.join(four_above) == 'XMAS':
-      count += 1
-
-  if i <= lower_bound:
-    four_below = []
-    for offset in range(0, 4):
-      four_below.append(rows[i + offset][j])
-    if ''.join(four_below) == 'XMAS':
-      count += 1
-
-  # Check diagonally [/] way
-  if i >= upper_bound and j <= right_bound:
-    four_upright = []
-    for offset in range(0, 4):
-      four_upright.append(rows[i - offset][j + offset])
-    if ''.join(four_upright) == 'XMAS':
-      count += 1
-
-  if i <= lower_bound and j >= left_bound:
-    four_downleft = []
-    for offset in range(0, 4):
-      four_downleft.append(rows[i + offset][j - offset])
-    if ''.join(four_downleft) == 'XMAS':
-      count += 1
-
-  # Check diagonally [\] way
-  if i >= upper_bound and j >= left_bound:
-    four_upleft = []
-    for offset in range(0, 4):
-      four_upleft.append(rows[i - offset][j - offset])
-    if ''.join(four_upleft) == 'XMAS':
-      count += 1
-
-  if i <= lower_bound and j <= right_bound:
-    four_downright = []
-    for offset in range(0, 4):
-      four_downright.append(rows[i + offset][j + offset])
-    if ''.join(four_downright) == 'XMAS':
-      count += 1
-
+  for direction in directions:
+    if check_direction(i, j, direction):
+        count += 1
   return count
 
+def check_direction(i, j, direction):
+  i_offset, j_offset = direction
+  checked_letters = []
+  for n in range(0, 4):
+    new_i = i + (i_offset * n)
+    new_j = j + (j_offset * n)
+    if new_i < upper_bound or new_i > lower_bound:
+      return False
+    if new_j < left_bound or new_j > right_bound:
+      return False
+    checked_letters.append(rows[new_i][new_j])
+  return ''.join(checked_letters) == 'XMAS'
 
 total_xmases = 0
 
