a9f54619a4893c53e58abc9a15e1e7839b1c0bb6
Fred Chien <fred.chien3@gmail.com>
2024-12-19 11:41:14 +0800

    Add day 18 part 1

--- day18/part1.py
+++ day18/part1.py
@@ -7,21 +7,28 @@ directions = {
   'v': ( 1,  0)
 }
 
-### Methods
-def drop_byte(position):
+##### Methods
+
+# Takes the byte that is up next in line, and drops it,
+# modifying the field.
+def drop_byte():
+  if not byte_positions: return
+  position = byte_positions.popleft()
   x, y = position
   space[y][x] = '#'
+  return position
 
 def render():
-  print('\n')
   for row in space:
     print(''.join(row))
+  print('\n')
 
 def in_bounds(pos):
   y, x = pos
   return ( y >= 0 and y < len(space) and
            x >= 0 and x < len(space[0]) )
 
+# Cosmetic function.
 def mark_traveled(pos):
   y, x = pos
   space[y][x] = 'O'
@@ -64,35 +71,58 @@ def get_path(start, end):
 
   return path
 
-###
+#####
 
 file = open('input.txt', 'r')
-
 byte_strings = file.read().split('\n')
-
 file.close()
 
-byte_positions = [
+byte_positions = deque([
   tuple(
     int(i) for i in byte_string.split(',')
   ) for byte_string in byte_strings
-]
+])
 
+### Input variables
+max_index = 70
+num_initial_bytes_to_drop = 1024
+###
 
-max_index = 6
 size = max_index + 1
 space = [['.' for _ in range(size)] for _ in range(size)]
 
-# Wait for the first 12 falling bytes
-for position in byte_positions[:12]:
-  drop_byte(position)
+# Wait for the first n falling bytes
+for _ in range(num_initial_bytes_to_drop):
+  drop_byte()
 
 render()
-# Wait one more - this simulates avoiding walking directly under a falling byte
-# drop_byte(byte_positions[13])
 
-# Now take a step towards the entrance and drop a byte.
-# Recalculate the path if a byte lands into it.
 pos = (0, 0)
 target = (max_index, max_index)
+num_steps = 0
+# Now take a step towards the entrance and drop a byte.
+# Recalculate the path if a byte lands into the path.
+
 path = get_path(pos, target)
+
+while path:
+  step = path.popleft()
+  num_steps += 1
+
+  mark_traveled(step)
+
+  dropped_byte = drop_byte()
+
+  if dropped_byte in path:
+    print(dropped_byte, ' recalculating')
+    path = get_path(step, target)
+    continue
+
+render()
+print(num_steps)
+
+# Addendum: Looks like I overengineered this solution.
+# Part 1 just calls for dropping 1024 bytes,
+# then calculating the length of the exit path.
+# No further bytes needed to be dropped while traversing the path.
+# Interestingly though my output is the same in both cases.
